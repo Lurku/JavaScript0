d1df5d994ab22077413870c78473782ea674b363
Lurker <80824757+Lurku@users.noreply.github.com>
2025-05-23 13:27:21 +0530

    Update 01_script.js

--- 01_script.js
+++ 01_script.js
@@ -1,7 +1,7 @@
 const accountId = 1234; // 'const' is used to declare a constant , it cannot be changed
 let accountName = "Mayank"; // 'let' is used to declare a variable , it can be reassigned(update value) but not redeclared(again declare using let)
-var accountPassword = "12345"; // 'var' is like let, but it can be redeclared , "PLZZ DON'T USE IT"
-accountCity = "Mumbai"; // this is also valid
+var accountPassword = "GAY "; // 'var' is like let, but it can be redeclared , "PLZZ DON'T USE IT"
+accountCity = "Diddy House"; // this is also valid
 let accountState; // variables can be declared without assigning any value
 
 console.log(accountId, accountName, accountPassword, accountCity, accountState); // linear output
